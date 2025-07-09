5bf5d7f99bd2d06f34e812626ab707e9b81b901f
Miguelz <62753251+Migueleo198@users.noreply.github.com>
2025-07-09 18:52:11 +0200

    commit gamepanel

--- Alzanarath/src/Main/GamePanel.java
+++ Alzanarath/src/Main/GamePanel.java
@@ -1,5 +1,25 @@
 package Main;
 
-public class GamePanel {
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	
+	//SCREEN SETTINGS
+	
+	final int tileSize = 48;
+	
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	
+	final int screenWidth = tileSize * maxScreenCol; //768p
+	final int screenHeight = tileSize * maxScreenRow; // 576p
+	
+	public GamePanel () {
+		this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+	}
 }

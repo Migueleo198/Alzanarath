2c2f2e72dd70d7d375668a01d0614f065f93efd3
Miguelz <62753251+Migueleo198@users.noreply.github.com>
2025-07-10 14:55:38 +0200

    COMMIT

--- Alzanarath/src/entity/Entity.java
+++ Alzanarath/src/entity/Entity.java
@@ -1,12 +1,13 @@
 package entity;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Entity {
 	
-	protected int x;
-	protected int y;
-	protected int speed;
+	public int worldX;
+	public int worldY;
+	public int speed;
 	
 	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
 	
@@ -14,6 +15,10 @@ public class Entity {
 	
 	public int spriteCounter = 0;
 	public int spriteNum = 1;
+	
+	public Rectangle solidArea;
+	public int solidAreaDefaultX, solidAreaDefaultY;
+	public boolean collisionOn = false;
 	
 	public Entity(){
 		
